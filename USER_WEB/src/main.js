ad4ec62151736b25420b9ae16cdfa342c1b34c14
PortikSzabolcs <portik.szabolcs@student.ms.sapientia.ro>
2023-10-12 19:14:01 +0300

    BUS-6 Added vuetify to frontend project

--- USER_WEB/src/main.js
+++ USER_WEB/src/main.js
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
+const vuetify = createVuetify()
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')

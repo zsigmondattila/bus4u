b1b2f094d2ef49a1349400cd3246d4dea8d34d44
Zsigmond Attila <zsigmond.attila@student.ms.sapientia.ro>
2023-10-21 22:43:05 +0300

    Created the first 6 tables and an nginx config file

--- API/db/migrate/20231012145933_devise_token_auth_create_users.rb
+++ API/db/migrate/20231012145933_devise_token_auth_create_users.rb
@@ -29,10 +29,11 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
+      t.string :firstname
+      t.string :lastname
       t.string :email
+      t.string :phone_number
+      t.string :language
 
       ## Tokens
       t.json :tokens
---
+++ API/db/migrate/20231021193116_create_routes.rb
@@ -0,0 +1,10 @@
+class CreateRoutes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :routes do |t|
+      t.string :route_uid
+      t.string :name
+      
+      t.timestamps
+    end
+  end
+end

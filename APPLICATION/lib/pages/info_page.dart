c867388a27036525251e228d8c0acb00fa45b542
BodoBalint <bodo.balint@student.ms.sapientia.ro>
2023-11-26 20:29:40 +0200

    Info page style

--- APPLICATION/lib/pages/info_page.dart
+++ APPLICATION/lib/pages/info_page.dart
@@ -12,21 +12,18 @@ class _InfoPageState extends State<InfoPage> {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-        
+      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
         Text(
           "Info",
-          style: TextStyle(color: Colors.black, fontSize: 20),
-          
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 24),
         ),
         SizedBox(height: 25),
         Text(
           "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
           style: TextStyle(color: Colors.black, fontSize: 16),
         ),
-        SizedBox(height: 350),
+        SizedBox(height: 300),
         Text(
           "Email: xxxxxxxxxx@xxxxx.xxx",
           style: TextStyle(color: Colors.black, fontSize: 16),
@@ -35,14 +32,14 @@ class _InfoPageState extends State<InfoPage> {
           "Tel: 0000000000",
           style: TextStyle(color: Colors.black, fontSize: 16),
         ),
-        SizedBox(height: 25),
+        SizedBox(height: 55),
         Text(
           "Created by: Bodo Balint, Portik Szabolcs, Zsigmond Attila",
-          style: TextStyle(color: Colors.black, fontSize: 16),
+          style: TextStyle(color: Colors.blueGrey, fontSize: 16),
         ),
         Text(
           "Sapientia EMTE 2023",
-          style: TextStyle(color: Colors.black, fontSize: 16),
+          style: TextStyle(color: Colors.blueGrey, fontSize: 16),
         ),
       ]),
     );

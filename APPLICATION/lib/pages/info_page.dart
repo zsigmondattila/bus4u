29dcbd879dfdb4bbcee539d24907e5de08920640
BodoBalint <bodo.balint@student.ms.sapientia.ro>
2023-11-26 20:29:40 +0200

    Created Info Page

--- APPLICATION/lib/pages/info_page.dart
+++ APPLICATION/lib/pages/info_page.dart
@@ -10,14 +10,41 @@ class InfoPage extends StatefulWidget {
 class _InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text(
-        "Info pageee",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+        
+        Text(
+          "Info",
+          style: TextStyle(color: Colors.black, fontSize: 20),
+          
         ),
-      ),
+        SizedBox(height: 25),
+        Text(
+          "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+        SizedBox(height: 350),
+        Text(
+          "Email: xxxxxxxxxx@xxxxx.xxx",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+        Text(
+          "Tel: 0000000000",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+        SizedBox(height: 25),
+        Text(
+          "Created by: Bodo Balint, Portik Szabolcs, Zsigmond Attila",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+        Text(
+          "Sapientia EMTE 2023",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+      ]),
     );
   }
 }

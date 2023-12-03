434b4bfa239720c6ab9ac8b03024de2efdbf01a1
Zsigmond Attila <zsigmond.attila@student.ms.sapientia.ro>
2023-12-03 22:55:14 +0200

    Made the POS login page

---
+++ POS_APP/lib/scan_ticket.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScanTicket extends StatefulWidget {
+  @override
+  _ScanTicketState createState() => _ScanTicketState();
+}
+
+class _ScanTicketState extends State<ScanTicket> {
+ 
+}

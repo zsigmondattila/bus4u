551ea4fcdb2c57da55bb25f1fb653776c6186cce
PortikSzabolcs <portik.szabolcs@student.ms.sapientia.ro>
2023-12-19 12:20:06 +0200

    BUS-49 Created buses page
    + create/delete buses

---
+++ ADMIN_WEB/src/components/BusCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card height="100%" class="d-flex flex-column" min-width="200">
+      <v-sheet class="text-center" color="primary">
+        <v-icon size="120" icon="mdi-bus"></v-icon>
+      </v-sheet>
+      <v-card-title class="text-center"> {{ bus.brand }} </v-card-title>
+      <v-card-subtitle class="flex-1-1">
+        <p>License plate: {{ bus.license_plate }}</p>
+        <p>Capacity: {{ bus.capacity }}</p>
+        <p>Manufactured: {{ bus.manufacturing_year }}</p>
+        <p>Road tax valid: {{ roadTax }}</p>
+        <p>Insurance valid: {{ insurance }}</p>
+        <p>Technical exam: {{ technicalExam }}</p>
+      </v-card-subtitle>
+      <v-card-actions class="justify-center">
+        <v-btn prepend-icon="mdi-trash-can-outline" color="red" @click="deletebus">
+          Delete
+        </v-btn>
+      </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['bus'])
+const emit = defineEmits(['delete'])
+
+const roadTax = computed(() => {
+  if(!props.bus.road_tax) return null
+  let date = new Date(props.bus.road_tax)
+  return date.toLocaleDateString()
+})
+
+const insurance = computed(() => {
+  if(!props.bus.insurance) return null
+  let date = new Date(props.bus.insurance)
+  return date.toLocaleDateString()
+})
+
+const technicalExam = computed(() => {
+  if(!props.bus.technical_exam) return null
+  let date = new Date(props.bus.technical_exam)
+  return date.toLocaleDateString()
+})
+
+function deletebus() {
+  emit('delete')
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ ADMIN_WEB/src/components/BusList.vue
@@ -0,0 +1,65 @@
+<template>
+  <SectionTitle>
+    Manage buses
+    <template #description>
+      Create or delete buses for your company
+    </template>
+  </SectionTitle>
+  <v-container>
+    <v-row>
+      <v-col v-for="bus in buses" :key="bus.bus_uid" cols="12" sm="6" md="4" lg="3" xl="2">
+        <BusCard :bus="bus" @delete="deleteBus(bus)"/>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+        <v-card :to="{name: 'create-bus'}" height="100%" class="d-flex flex-column text-center" min-width="200">
+          <v-sheet class="text-center" color="adjacent">
+            <v-icon size="120" icon="mdi-plus-circle-outline"></v-icon>
+          </v-sheet>
+          <v-spacer></v-spacer>
+          <v-card-title> Add new </v-card-title>
+          <v-spacer></v-spacer>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-snackbar v-model="isDeleted">
+    Bus deleted successfully
+  </v-snackbar>
+  <v-snackbar v-model="error">
+    Operation failed
+  </v-snackbar>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import BusCard from '../components/BusCard.vue';
+import SectionTitle from './SectionTitle.vue';
+import { userStore } from '../stores/userStore';
+
+const user = userStore()
+const buses = ref([])
+const isDeleted = ref(false)
+const error = ref(false)
+
+function deleteBus(bus) {
+  console.log(bus);
+  axios.delete('https://bus4u.fast-table.com/v1/admin/delete_bus', { params: { company_uid: user.company_uid, bus_uid: bus.bus_uid }})
+  .then(() => {
+    isDeleted.value = true
+    axios.get('https://bus4u.fast-table.com/v1/admin/get_buses_of_a_company', { params: { company_uid: user.company_uid }})
+      .then(rsp => {
+        buses.value = rsp.data
+      }).catch(() => buses.value = [])
+  }).catch(() => error.value = true)
+}
+
+axios.get('https://bus4u.fast-table.com/v1/admin/get_buses_of_a_company', { params: { company_uid: user.company_uid }})
+  .then(rsp => {
+    buses.value = rsp.data
+  }).catch(() => buses.value = [])
+</script>
+
+<style scoped>
+
+</style>
